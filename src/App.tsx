8acc9a2174027081a5c418c0cf9f746768bfa83d
Onurcan Sever <onsever@icloud.com>
2023-07-06 13:42:25 -0400

    initialize react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { JSX } from "react";
+
+const App: React.FC = (): JSX.Element => {
+    return (
+        <div>
+        <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default App;
